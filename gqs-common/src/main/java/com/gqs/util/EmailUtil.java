a6f7a63edcfbaa1d9fdaaa7901216a46402a2f0c
guoqiaosen <guoqiaosen@besunyen.com>
2021-09-22 10:24:04 +0800

    email校验工具类

---
+++ gqs-common/src/main/java/com/gqs/util/EmailUtil.java
@@ -0,0 +1,28 @@
+package com.gqs.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author jiabochao
+ */
+public class EmailUtil {
+
+    /**
+     * 验证邮箱
+     * @param email
+     * @return
+     */
+    public static boolean checkEmail(String email) {
+        boolean flag = false;
+        try {
+            String check = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+            Pattern regex = Pattern.compile(check);
+            Matcher matcher = regex.matcher(email);
+            flag = matcher.matches();
+        } catch (Exception e) {
+            flag = false;
+        }
+        return flag;
+    }
+}
